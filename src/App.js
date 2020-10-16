bd3089e5af4fea422a1d13e61df9e9ff6449d962
olaouso <ouso.ola@gmail.com>
2020-10-16 15:38:41 +0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-/* eslint-disable no-unused-vars */
+
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Completed from "./components/Completed";
-import Nav from "./components/Nav";
+import NavBar1 from "./components/Nav";
 
 function App() {
   return (
     <Router>
-      {/* <Nav className="justify-content-center" activeKey="/home"/> */}
-      <Route path="/" component={Nav} />
+      <Route path="/" component={NavBar1} />
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/completed" component={Completed} />
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,46 +1,49 @@
 import React from "react";
 // import "../App.css";
+// import logo from "./logo.png";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
-const Nav = () => {
-  return (
-    <div className="navbar">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/completed">Completed</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
-};
-export default Nav;
 
-// (
-//   <Nav
-//     activeKey="/home"
-//     onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-//   >
-//     <Nav.Item>
-//       <Nav.Link href="/home">Active</Nav.Link>
-//     </Nav.Item>
-//     <Nav.Item>
-//       <Nav.Link eventKey="link-1">Link</Nav.Link>
-//     </Nav.Item>
-//     <Nav.Item>
-//       <Nav.Link eventKey="link-2">Link</Nav.Link>
-//     </Nav.Item>
-//     <Nav.Item>
-//       <Nav.Link eventKey="disabled" disabled>
-//         Disabled
-//       </Nav.Link>
-//     </Nav.Item>
-//   </Nav>
-// );
+const NavBar1 = () => {
+  return (
+    // <div className="navbar">
+    //   <img src="../boardlogo.jpg"></img>
+    //   <nav>
+    //     <ul>
+    //       <li>
+    //         <Link to="/">Home</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/about">About</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/completed">Completed</Link>
+    //       </li>
+    //     </ul>
+    //   </nav>
+    // </div>
+ <Navbar bg="light" expand="lg">
+  <Navbar.Brand src="C:\Users\ouol\Desktop\ReCoded\Board project\Board-Project\src\components\Nav\logo.png"></Navbar.Brand>
+  {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+  <Navbar.Collapse id="basic-navbar-nav">
+   <img src="/image/logo.jpg" alt="logo" width= "100px" ></img> 
+   
+   <Nav  className="mr-auto">
+      <Link to="/" >Home</Link>
+      <Link to="/about" >About</Link>
+      <Link to="/completed" >Completed</Link>
+      {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Home</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">About</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Completed</NavDropdown.Item>
+        <NavDropdown.Divider />
+      </NavDropdown> */}
+    </Nav>
+  </Navbar.Collapse>
+</Navbar> 
+ );
+};
+export default NavBar1;
+
